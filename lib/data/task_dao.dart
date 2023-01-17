64723b4442d2fa3ffa3c2dfe2192b4851b0dbaa2
julia.cavalcante <ccardozojulia@gmail.com>
2023-01-17 14:24:20 -0300

    Creating findAll method

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:nosso_primeiro_projeto/components/task.dart';
+import 'package:nosso_primeiro_projeto/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String tableSql = 'CREATE TABLE $_tablename('
@@ -12,7 +14,24 @@ class TaskDao {
   static const String _image = 'image';
 
   save(Task tarefa) async {}
-  Future<List<Task>> findAll() async{}
+
+  Future<List<Task>> findAll() async{
+    print('acessando findAll: ');
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result = await bancoDeDados.query(_tablename);
+    print('$result');
+    return toList(result);
+  }
+  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas){
+    final List<Task> tarefas = [];
+    for(Map<String, dynamic> linha in mapaDeTarefas){
+      final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
+      tarefas.add(tarefa);
+    }
+    return tarefas;
+  }
+
   Future<List<Task>> find(String nomeDaTarefa) async{}
+
   delete(String nomeDaTarefa) async{}
 }
