3e7a82ce3e633d40141674bb55bada2e0f63b598
julia.cavalcante <ccardozojulia@gmail.com>
2023-01-17 15:32:32 -0300

    Creating save and update method

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -13,7 +13,26 @@ class TaskDao {
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
 
-  save(Task tarefa) async {}
+  save(Task tarefa) async {
+    final Database bancoDeDados = await getDatabase();
+    var itemExists = await find(tarefa.nome);
+    Map<String, dynamic> taskMap = toMap(tarefa);
+    if (itemExists.isEmpty) {
+      return await bancoDeDados.insert(_tablename, taskMap);
+    } else {
+      return await bancoDeDados.update(_tablename, taskMap,
+          where: '$_name = ?', whereArgs: [tarefa.nome]);
+    }
+  }
+
+  Map<String, dynamic> toMap(Task tarefa) {
+    final Map<String, dynamic> mapaDeTarefas = Map();
+    mapaDeTarefas[_name] = tarefa.nome;
+    mapaDeTarefas[_difficulty] = tarefa.dificuldade;
+    mapaDeTarefas[_image] = tarefa.foto;
+    print('$mapaDeTarefas');
+    return mapaDeTarefas;
+  }
 
   Future<List<Task>> findAll() async {
     print('acessando findAll: ');
@@ -27,7 +46,11 @@ class TaskDao {
   List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas) {
     final List<Task> tarefas = [];
     for (Map<String, dynamic> linha in mapaDeTarefas) {
-      final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
+      final Task tarefa = Task(
+        linha[_name],
+        linha[_image],
+        linha[_difficulty],
+      );
       tarefas.add(tarefa);
     }
     return tarefas;
@@ -35,10 +58,20 @@ class TaskDao {
 
   Future<List<Task>> find(String nomeDaTarefa) async {
     final Database bancoDeDados = await getDatabase();
-    final List<Map<String, dynamic>> result = await bancoDeDados
-        .query(_tablename, where: '$_name = ?', whereArgs: [nomeDaTarefa]);
+    final List<Map<String, dynamic>> result = await bancoDeDados.query(
+      _tablename,
+      where: '$_name = ?',
+      whereArgs: [nomeDaTarefa],
+    );
     return toList(result);
   }
 
-  delete(String nomeDaTarefa) async {}
+  delete(String nomeDaTarefa) async {
+    final Database bancoDeDados = await getDatabase();
+    return bancoDeDados.delete(
+      _tablename,
+      where: '$_name = ?',
+      whereArgs: [nomeDaTarefa],
+    );
+  }
 }
