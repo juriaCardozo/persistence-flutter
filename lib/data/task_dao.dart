852d9f1582449008347fde0c7c305fd3429d4500
julia.cavalcante <ccardozojulia@gmail.com>
2023-01-17 14:47:50 -0300

    Creating find method

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -15,23 +15,30 @@ class TaskDao {
 
   save(Task tarefa) async {}
 
-  Future<List<Task>> findAll() async{
+  Future<List<Task>> findAll() async {
     print('acessando findAll: ');
     final Database bancoDeDados = await getDatabase();
-    final List<Map<String, dynamic>> result = await bancoDeDados.query(_tablename);
+    final List<Map<String, dynamic>> result =
+        await bancoDeDados.query(_tablename);
     print('$result');
     return toList(result);
   }
-  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas){
+
+  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas) {
     final List<Task> tarefas = [];
-    for(Map<String, dynamic> linha in mapaDeTarefas){
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
       final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
       tarefas.add(tarefa);
     }
     return tarefas;
   }
 
-  Future<List<Task>> find(String nomeDaTarefa) async{}
+  Future<List<Task>> find(String nomeDaTarefa) async {
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result = await bancoDeDados
+        .query(_tablename, where: '$_name = ?', whereArgs: [nomeDaTarefa]);
+    return toList(result);
+  }
 
-  delete(String nomeDaTarefa) async{}
+  delete(String nomeDaTarefa) async {}
 }
